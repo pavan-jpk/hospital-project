77858034f72c2a93b2e3aee086f43750a3c9fc9d
Pawan <jallapavankumar99@gmail.com>
2023-12-15 16:40:46 +0530

    pro commit

---
+++ src/main/java/com/org/helper/ConnectionHelper.java
@@ -0,0 +1,21 @@
+package com.org.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionHelper 
+{
+  public static Connection getConObj()
+  {
+	  try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital","root","root");
+		return con;
+	} catch (ClassNotFoundException | SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	  return null;
+  }
+}
